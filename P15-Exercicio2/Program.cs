14cb6a5d8e5d14fb0357affc72a28ef011e24079
Andre Goltz <andregoltz@outlook.com.br>
2021-04-01 22:37:44 -0300

    Add files via upload

---
+++ P15-Exercicio2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P15_Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 15 - Exercícios 2");
+
+            for (int contador = 1; contador <= 100; contador++)
+            {
+                if (contador % 3 == 0)
+                {
+                    Console.WriteLine(contador);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
